bc53d9df527c271bbaa8b21dc1fcf619697ea694
Shinto Philip <shinto.philip007@gmail.com>
2020-11-01 15:19:36 +0530

    bootstrap react project

---
+++ webpack.config.dev.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+process.env.NODE_ENV = 'development';
+
+module.exports = {
+  mode: 'development', // for dev env
+  target: 'web',
+  devtool: 'cheap-module-source-map', // helps in displaying actual code during debug in browser
+  entry: './src/index', // entry script
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    stats: 'minimal',
+    overlay: true,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    https: false,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      favicon: 'src/favicon.ico',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        // to run babel for all JS code
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /(\.css)$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
